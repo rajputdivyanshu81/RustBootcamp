b89aeb2f5ba9494bc67e2c38f7b89f51d3db585c
Divyanshu Rajput <divyanshurajput709@gmail.com>
2024-08-04 13:04:22 +0530

    Add files via upload

---
+++ src/tempCodeRunnerFile.rs
@@ -0,0 +1,19 @@
+
+// #[derive(Debug)]
+// enum Direction {
+//     North,
+//     East,
+//     South,
+//     West,
+// }
+
+// fn main() {
+//     let my_direction = Direction::North;
+//     let new_direction = my_direction; // No error, because Direction is Copy
+//     move_around(new_direction);
+// }
+
+// fn move_around(direction: Direction) {
+//     println!("{:?}", direction); // Print the direction for debugging
+//     // Implement logic to move a character around
+// }
